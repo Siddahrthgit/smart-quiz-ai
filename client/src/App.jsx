74fc5f523fbe848ddcac08c21d217a8392c6f938
Siddarthgit <yadavsiddharth014@gmail.com>
2026-07-30 19:18:19 +0545

    add frontend pages and backend fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+function App() {
+  return (
+    <div>
+      <h1>Smart Quiz AI 🚀</h1>
+      <p>Your frontend is working!</p>
+    </div>
+  );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+export default App;
